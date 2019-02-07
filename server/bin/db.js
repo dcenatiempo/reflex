b963a6b191c4aa178376005c2149646e5c1bdb92
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2019-02-07 08:05:43 -0700

    Add rooms and users collections to db

--- server/bin/db.js
+++ server/bin/db.js
@@ -3,6 +3,8 @@ const MongoClient = require('mongodb').MongoClient;
 
 const state = {
   db: null,
+  rooms: null,
+  users: null,
 }
 const options = { useNewUrlParser: true };
 
@@ -10,13 +12,15 @@ exports.connect = function(url, done) {
   if (state.db) return done()
   MongoClient.connect(url, options, function(err, db) {
     if (err) return done(err)
-    state.db = db
+    state.db = db.db('reflex');
+    state.rooms = state.db.collection('rooms');
+    state.users = state.db.collection('users');
     done()
   })
 }
 
 exports.get = function() {
-  return state.db
+  return state;
 }
 
 exports.close = function(done) {
