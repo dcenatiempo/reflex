9866762e4bcea4c0861147f093d8e530a1cbd5fc
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2019-04-11 19:34:39 -0600

    Push dist folder to heroku

---
+++ client/dist/precache-manifest.891916827b540f25985cbefb77cf9e4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ea796423e8b46a56e826",
+    "url": "/js/chunk-vendors.c60e2c94.js"
+  },
+  {
+    "revision": "b01cbcd7c37a6097c9e5",
+    "url": "/js/app.aaee3248.js"
+  },
+  {
+    "revision": "c5169826e8cd69bd19db",
+    "url": "/js/about.409d3981.js"
+  },
+  {
+    "revision": "1ced84894c1705344c66e215bc4bec9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b01cbcd7c37a6097c9e5",
+    "url": "/css/app.091e2c43.css"
+  },
+  {
+    "revision": "c5169826e8cd69bd19db",
+    "url": "/css/about.4b1b9a3b.css"
+  }
+];
